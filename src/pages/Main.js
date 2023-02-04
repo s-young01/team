5483f9d81082dbb93abbc192ea917df45e10291b
namminimi <tbvhdaos@naver.com>
2023-02-04 22:36:07 +0900

    nam movie
    change

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,12 +2,20 @@ import React from 'react';
 import './Main.scss';
 import Latest from './Latest';
 import Recomend from './Recomend';
+import NewMovie from './NewMovie';
+
+
+    
+   /*  const hidden = () => {
+        style: {display: {sct} == 600 ? "none": "block"}
+    } */
 
 const Main = () => {
     return (
-        <div className='main'>
+        <div className='main' >
+            <NewMovie/>
             <Latest/>
-            <Recomend/>
+            <Recomend/>    
         </div>
     );
 };
