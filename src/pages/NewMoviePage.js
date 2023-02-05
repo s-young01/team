8b30e6628222fad57030745c5ca07df178e8dc84
fedfs900 <ckdals900@gmail.com>
2023-02-05 21:13:51 +0900

    s-young01
    안녕
    
    Co-Authored-By: s-young01 <119985133+s-young01@users.noreply.github.com>

---
+++ src/pages/NewMoviePage.js
@@ -0,0 +1,133 @@
+import React from 'react';
+import "./NewMoviePage.scss";
+const NewMoviePage = () => {
+    return (
+        <div className="NewMovie_page">
+            <h2>최신영화 - 현재 상영작</h2>
+            <div className="NewMovie_box">
+            <div className="NewMovie">
+                <div className="NewMovie_img_div"><img src="images/poster1_1.jpg" className="NewMovie_img"/></div>
+                <div className="NewMovie_desc">
+                    <h4 className="NewMovie_title">영화 제목</h4>
+                    <p className="NewMovie_subdesc">영화 스토리</p>
+                    <div className="NewMovie_score">평점 10.0</div>
+                </div>
+            </div>
+            <div className="NewMovie">
+                <div className="NewMovie_img_div"><img src="images/poster1_2.jpg" className="NewMovie_img"/></div>
+                <div className="NewMovie_desc">
+                    <h4 className="NewMovie_title">영화 제목</h4>
+                    <p className="NewMovie_subdesc">영화 스토리</p>
+                    <div className="NewMovie_score">평점 10.0</div>
+                </div>
+            </div>
+            <div className="NewMovie">
+                <div className="NewMovie_img_div"><img src="images/poster1_3.jpg" className="NewMovie_img"/></div>
+                <div className="NewMovie_desc">
+                    <h4 className="NewMovie_title">영화 제목</h4>
+                    <p className="NewMovie_subdesc">영화 스토리</p>
+                    <div className="NewMovie_score">평점 10.0</div>
+                </div>
+            </div>
+            <div className="NewMovie">
+                <div className="NewMovie_img_div"><img src="images/poster1_4.jpg" className="NewMovie_img"/></div>
+                <div className="NewMovie_desc">
+                    <h4 className="NewMovie_title">영화 제목</h4>
+                    <p className="NewMovie_subdesc">영화 스토리</p>
+                    <div className="NewMovie_score">평점 10.0</div>
+                </div>
+            </div>
+            <div className="NewMovie">
+                <div className="NewMovie_img_div"><img src="images/poster1_5.jpg" className="NewMovie_img"/></div>
+                <div className="NewMovie_desc">
+                    <h4 className="NewMovie_title">영화 제목</h4>
+                    <p className="NewMovie_subdesc">영화 스토리</p>
+                    <div className="NewMovie_score">평점 10.0</div>
+                </div>
+            </div>
+            <div className="NewMovie">
+                <div className="NewMovie_img_div"><img src="images/poster1_6.jpg" className="NewMovie_img"/></div>
+                <div className="NewMovie_desc">
+                    <h4 className="NewMovie_title">영화 제목</h4>
+                    <p className="NewMovie_subdesc">영화 스토리</p>
+                    <div className="NewMovie_score">평점 10.0</div>
+                </div>
+            </div>
+            <div className="NewMovie">
+                <div className="NewMovie_img_div"><img src="images/poster1_7.jpg" className="NewMovie_img"/></div>
+                <div className="NewMovie_desc">
+                    <h4 className="NewMovie_title">영화 제목</h4>
+                    <p className="NewMovie_subdesc">영화 스토리</p>
+                    <div className="NewMovie_score">평점 10.0</div>
+                </div>
+            </div>
+            <div className="NewMovie">
+                <div className="NewMovie_img_div"><img src="images/poster1_8.jpg" className="NewMovie_img"/></div>
+                <div className="NewMovie_desc">
+                    <h4 className="NewMovie_title">영화 제목</h4>
+                    <p className="NewMovie_subdesc">영화 스토리</p>
+                    <div className="NewMovie_score">평점 10.0</div>
+                </div>
+            </div>
+            <div className="NewMovie">
+                <div className="NewMovie_img_div"><img src="images/poster1_9.jpg" className="NewMovie_img"/></div>
+                <div className="NewMovie_desc">
+                    <h4 className="NewMovie_title">영화 제목</h4>
+                    <p className="NewMovie_subdesc">영화 스토리</p>
+                    <div className="NewMovie_score">평점 10.0</div>
+                </div>
+            </div>
+            <div className="NewMovie">
+                <div className="NewMovie_img_div"><img src="images/poster1_10.jpg" className="NewMovie_img"/></div>
+                <div className="NewMovie_desc">
+                    <h4 className="NewMovie_title">영화 제목</h4>
+                    <p className="NewMovie_subdesc">영화 스토리</p>
+                    <div className="NewMovie_score">평점 10.0</div>
+                </div>
+            </div>
+            <div className="NewMovie">
+                <div className="NewMovie_img_div"><img src="images/poster1_11.jpg" className="NewMovie_img"/></div>
+                <div className="NewMovie_desc">
+                    <h4 className="NewMovie_title">영화 제목</h4>
+                    <p className="NewMovie_subdesc">영화 스토리</p>
+                    <div className="NewMovie_score">평점 10.0</div>
+                </div>
+            </div>
+            <div className="NewMovie">
+                <div className="NewMovie_img_div"><img src="images/poster1_12.jpg" className="NewMovie_img"/></div>
+                <div className="NewMovie_desc">
+                    <h4 className="NewMovie_title">영화 제목</h4>
+                    <p className="NewMovie_subdesc">영화 스토리</p>
+                    <div className="NewMovie_score">평점 10.0</div>
+                </div>
+            </div>
+            <div className="NewMovie">
+                <div className="NewMovie_img_div"><img src="images/poster1_13.jpg" className="NewMovie_img"/></div>
+                <div className="NewMovie_desc">
+                    <h4 className="NewMovie_title">영화 제목</h4>
+                    <p className="NewMovie_subdesc">영화 스토리</p>
+                    <div className="NewMovie_score">평점 10.0</div>
+                </div>
+            </div>
+            <div className="NewMovie">
+                <div className="NewMovie_img_div"><img src="images/poster1_14.jpg" className="NewMovie_img"/></div>
+                <div className="NewMovie_desc">
+                    <h4 className="NewMovie_title">영화 제목</h4>
+                    <p className="NewMovie_subdesc">영화 스토리</p>
+                    <div className="NewMovie_score">평점 10.0</div>
+                </div>
+            </div>
+            <div className="NewMovie">
+                <div className="NewMovie_img_div"><img src="images/poster1_15.jpg" className="NewMovie_img"/></div>
+                <div className="NewMovie_desc">
+                    <h4 className="NewMovie_title">영화 제목</h4>
+                    <p className="NewMovie_subdesc">영화 스토리</p>
+                    <div className="NewMovie_score">평점 10.0</div>
+                </div>
+            </div>
+            </div>
+        </div>
+    );
+};
+
+export default NewMoviePage;
